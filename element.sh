fc138822c843cabd392bafd5a4d0b81d7ecf5cf4
Jeriken <59458237+jeriken@users.noreply.github.com>
2026-06-27 21:46:58 -0400

    fix: correct output format for element lookup

--- element.sh
+++ element.sh
@@ -1,8 +1,3 @@
-cd ~/project
-mkdir -p periodic_table && cd periodic_table
-git init && git checkout -b main
-
-cat > element.sh << 'EOF'
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
@@ -19,23 +14,11 @@ else
   WHERE="e.name='$1'"
 fi
 
-read -r AN NAME SYM TYPE MASS MELT BOIL <<< $(IFS='|'; echo $($PSQL "SELECT e.atomic_number,e.name,e.symbol,t.type,p.atomic_mass,p.melting_point_celsius,p.boiling_point_celsius FROM elements e JOIN properties p USING(atomic_number) JOIN types t USING(type_id) WHERE $WHERE"))
+RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p USING(atomic_number) JOIN types t USING(type_id) WHERE $WHERE")
 
-if [[ -z $AN ]]; then
+if [[ -z $RESULT ]]; then
   echo "I could not find that element in the database."
 else
+  IFS='|' read -r AN NAME SYM TYPE MASS MELT BOIL <<< "$RESULT"
   echo "The element with atomic number $AN is $NAME ($SYM). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
 fi
-EOF
-
-chmod +x element.sh
-
-git add .
-git commit -m "Initial commit"
-echo "# v2" >> element.sh && git add . && git commit -m "feat: add element lookup"
-echo "# v3" >> element.sh && git add . && git commit -m "fix: handle no argument"
-echo "# v4" >> element.sh && git add . && git commit -m "refactor: simplify query"
-echo "# v5" >> element.sh && git add . && git commit -m "chore: finalize script"
-
-pg_dump -cC --inserts -U freecodecamp periodic_table > periodic_table.sql
-git add . && git commit -m "chore: add database dump"
